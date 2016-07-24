af8effea01b31e3d44b983cc9ff208b43a3913ec
Tickii <admin@tickii.com>
2016-07-24 18:59:10 +0200

    Update debian-node-install.sh

--- debian-node-install.sh
+++ debian-node-install.sh
@@ -1,35 +1,4 @@
 #!/bin/bash
-function basic_packages {
-	echo "Installing lisk prerequisites and default Debain set..."
-	apt-get -y install ufw curl wget tar unzip zip sudo linux-headers-$(uname -r) mc joe dos2unix g++ gcc make tcpdump ngrep elinks git openssl ntp wget curl
-	echo "Packages installed"
-}
-
-function create_users {
-
-	echo -n "Adding wheel group and permissions..."
-	
-	addgroup wheel
-	bash -c 'echo "%wheel ALL = (ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)'
-	bash -c 'echo "%sudo ALL = (ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)'
-	
-	echo -n "Adding users..."
-	
-	for user in $USERLIST;
-	do
-	        echo -n "$user..."
-	        useradd -G wheel -s /bin/bash -m $user
-	        mkdir /home/$user/.ssh
-	        echo ${SSHKEYS[$user]} > /home/$user/.ssh/authorized_keys
-	        chown $user:$user /home/$user/.ssh
-	        chown $user:$user /home/$user/.ssh/authorized_keys
-	        chmod 700 /home/$user/.ssh
-	        chmod 600 /home/$user/.ssh/authorized_keys
-	done
-	
-	echo "Done with users..."
-}
-
 declare LISKNET
 
 if [[ "$@" == "main" ]]
@@ -50,33 +19,49 @@ SSHKEYS[kj]="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAxPXpcIzDwNb2PaDfGaq8n8j9C6Uu4Gh
 SSHKEYS[jb]="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAhDyHV7vzvS7/MfqsoanrEkZc84mxnHisz187ri4xFQ6iXuW2tZscf8gWizh6Mw4DZ8caZbEoZBKhtXH3Yhj2ZjF1T0jHjFzsgE3603YWVkIS5Z/pAih2wm0e+7X5HYK1x60nuMyPXtGv1aJM6aAvhkNFfA1Hn1mbqZF3YDyBEy0VsR8IweIAMF6ESA3g4Cvut0lVnCxrDgbyTG3RMHKoyF8ozK/lQfbDlKVi5bsMaq6/pi6iPzCqE9LQoUeaeXH/LPweZUjgKIh9GgBw0rsRTezIUHvwGh+97hoaZZaSaYuRw551Afx54juH0exj5FWwoGOg/9fNIkxcLzrcxeDWCbY1Y8Qr5+tb6dpfXgDBuNUvka0uYRF70CktCVl3ZVCG06s1rwaXbdxDdZKt1KrfzZvhZ2LGDKdLmxxqGgDWTSN13xoJab35bAXLWdi7qeTnPAoNlxbyajVWX2YEQxqpxHMdr0cdfGzY2UmVmPI91cZ0NDnC2CjQkOd3ZXE4O+PpdWX/rA/uj+fsA1faaPjXF29okbBzwBjnrKEGEiOg56is+BEFNVF2BgapPoZwIbrtW49TbDnhTzq5UTNWEK1WpqS5xKhro3nM8QVVCqOSmPajLMl3MJQ8f7esdYfctc6qxmz/kLEmrJBCvhmSseqgvjgJ7HCYwU5waSeKv1AGmXk= jb@tickii.com"
 SSHKEYS[lisk]="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAhDyHV7vzvS7/MfqsoanrEkZc84mxnHisz187ri4xFQ6iXuW2tZscf8gWizh6Mw4DZ8caZbEoZBKhtXH3Yhj2ZjF1T0jHjFzsgE3603YWVkIS5Z/pAih2wm0e+7X5HYK1x60nuMyPXtGv1aJM6aAvhkNFfA1Hn1mbqZF3YDyBEy0VsR8IweIAMF6ESA3g4Cvut0lVnCxrDgbyTG3RMHKoyF8ozK/lQfbDlKVi5bsMaq6/pi6iPzCqE9LQoUeaeXH/LPweZUjgKIh9GgBw0rsRTezIUHvwGh+97hoaZZaSaYuRw551Afx54juH0exj5FWwoGOg/9fNIkxcLzrcxeDWCbY1Y8Qr5+tb6dpfXgDBuNUvka0uYRF70CktCVl3ZVCG06s1rwaXbdxDdZKt1KrfzZvhZ2LGDKdLmxxqGgDWTSN13xoJab35bAXLWdi7qeTnPAoNlxbyajVWX2YEQxqpxHMdr0cdfGzY2UmVmPI91cZ0NDnC2CjQkOd3ZXE4O+PpdWX/rA/uj+fsA1faaPjXF29okbBzwBjnrKEGEiOg56is+BEFNVF2BgapPoZwIbrtW49TbDnhTzq5UTNWEK1WpqS5xKhro3nM8QVVCqOSmPajLMl3MJQ8f7esdYfctc6qxmz/kLEmrJBCvhmSseqgvjgJ7HCYwU5waSeKv1AGmXk= lisk@tickii.com"
 
-basic_packages
-create_users
+function basic_packages {
+	
+	echo "Installing lisk prerequisites and default Debain set..."
+	apt-get -y install ufw curl wget tar unzip zip sudo linux-headers-$(uname -r) mc joe dos2unix g++ gcc make tcpdump ngrep elinks git openssl ntp wget curl
+	echo "Packages installed"
+	
+	echo "Setting locale"
+	locale-gen en_US.UTF-8
+	update-locale LANG=en_US.UTF-8
+}
 
-echo -n "Updating SSHd configuration..."
-sed -i.old -e "s/^Port 22$/Port 222/" -e "s/^PermitRootLogin without-password/PermitRootLogin no/" -e "s/^PermitRootLogin yes/PermitRootLogin no/"  -e "s/^#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
-echo "Done"
+function create_users {
 
-echo "Restarting SSHd..."
-/etc/init.d/ssh restart
-
-echo "Setting up firewall.."
-
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow 222/tcp
-
-	read -p "Do you want to insall Lisk? <y/N> " prompt
-	if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-	then
+	echo "Adding users and permissions..."
+	
+	addgroup wheel
+	bash -c 'echo "%wheel ALL = (ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)'
+	bash -c 'echo "%sudo ALL = (ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)'
+	
+	for user in $USERLIST;
+	do
+	        echo -n "$user..."
+	        useradd -G wheel -s /bin/bash -m $user
+	        mkdir /home/$user/.ssh
+	        echo ${SSHKEYS[$user]} > /home/$user/.ssh/authorized_keys
+	        chown $user:$user /home/$user/.ssh
+	        chown $user:$user /home/$user/.ssh/authorized_keys
+	        chmod 700 /home/$user/.ssh
+	        chmod 600 /home/$user/.ssh/authorized_keys
+	done
+	
+	echo "Done with users..."
+}
+	
+function lisk {
 	
 	echo "Using nodejs v0.12"
 	curl -sL https://deb.nodesource.com/setup_0.12 | sudo -E bash -
-
+	
 	apt-get install -y nodejs
 	
 	echo -n "Adding wheel group and permissions for lisk..."
-
+	
 	useradd -G wheel -s /bin/bash -m lisk
 	mkdir /home/lisk
 	mkdir /home/lisk/.ssh
@@ -84,9 +69,6 @@ sudo ufw allow 222/tcp
 	chown -R lisk:lisk /home/lisk
 	chmod 700 /home/lisk/.ssh
 	chmod 600 /home/lisk/.ssh/authorized_keys
-			
-	locale-gen en_US.UTF-8
-	update-locale LANG=en_US.UTF-8
 	
 	cd /home/lisk
 	sudo -u lisk wget https://downloads.lisk.io/scripts/installLisk.sh
@@ -107,51 +89,80 @@ sudo ufw allow 222/tcp
 	sudo -u lisk tar xfv lisk-node-Linux-x86_64.tar.gz
 	sudo -u lisk bash lisk.sh start
 	
-		read -p "Do you want to create a Dapp now? <y/N> " prompt
-
-		if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-		then
-
-			echo "Installing Dapp..."
-			echo "Installing lisk Dapp tools"
-			npm install -g lisk-cli
-			npm install -g crypto-browserify
-			npm install -g browserify-bignum
+	read -p "Do you want to create a Dapp now? <y/N> " prompt
+	
+	if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+	then
+	
+		echo "Installing Dapp..."
+		echo "Installing lisk Dapp tools"
+		npm install -g lisk-cli
+		npm install -g crypto-browserify
+		npm install -g browserify-bignum
 		
-			sudo -u lisk bash lisk.sh stop
-			sudo -u lisk ssh-keygen -t rsa -b 4096 -C "lisk@tickii.com"
-			cat /home/lisk/.ssh/id_rsa.pub
-			read -n1 -r -p "Copy above key and insert into allowed ssh keys in tickii repo..." key
-			echo "Repo is located here: git@github.com:tickii/some-name-repo.git"
-			sudo -u lisk lisk-cli dapps -a
-			sudo -u lisk bash lisk.sh start
-			echo "Resetting database..."
-			killall node
-			dropdb -U lisk lisk_$LISKNET
-			createdb -U lisk lisk_$LISKNET
-			sudo -u lisk bash lisk.sh restart
-		fi
-		
-		if [[ $LISKNET == "main" ]]
-		then
-			sudo ufw allow 8000/tcp
-		else
-			sudo ufw allow 7000/tcp
-		fi
-		
-		echo "Done with Lisk..."
+		sudo -u lisk bash lisk.sh stop
+		sudo -u lisk ssh-keygen -t rsa -b 4096 -C "lisk@tickii.com"
+		cat /home/lisk/.ssh/id_rsa.pub
+		read -n1 -r -p "Copy above key and insert into allowed ssh keys in tickii repo..." key
+		echo "Repo is located here: git@github.com:tickii/some-name-repo.git"
+		sudo -u lisk lisk-cli dapps -a
+		sudo -u lisk bash lisk.sh start
+		echo "Resetting database..."
+		killall node
+		dropdb -U lisk lisk_$LISKNET
+		createdb -U lisk lisk_$LISKNET
+		sudo -u lisk bash lisk.sh restart
 	fi
+		
+	if [[ $LISKNET == "main" ]]
+	then
+		sudo ufw allow 8000/tcp
+	else
+		sudo ufw allow 7000/tcp
+	fi
+	
+	sudo ufw enable
+		
+	echo "Done with Lisk..."
+}
 
-echo "Setting up trusted IPs in firewall.."
+function security {
 
-for ipv4address in $TRUSTED_IPV4;
-do
-	echo "Allowing from $ipv4address..."
-	sudo ufw allow from $ipv4address
-       
-done
+	echo -n "Updating SSHd configuration..."
+	sed -i.old -e "s/^Port 22$/Port 222/" -e "s/^PermitRootLogin without-password/PermitRootLogin no/" -e "s/^PermitRootLogin yes/PermitRootLogin no/"  -e "s/^#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+	echo "Done"
+	
+	echo "Restarting SSHd..."
+	/etc/init.d/ssh restart
+	
+	echo "Setting up firewall.."
+	
+	ufw default deny incoming
+	ufw default allow outgoing
+	ufw allow 222/tcp
+	
+	echo "Setting up trusted IPs in firewall.."
+	for ipv4address in $TRUSTED_IPV4;
+	do
+		echo "Allowing from $ipv4address..."
+		sudo ufw allow from $ipv4address
+	       
+	done
+	
+	echo "Applying firewall..."
+	ufw enable
 
-echo "Applying firewall..."
-sudo ufw enable
+}
+
+
+basic_packages
+create_users
+security
+
+read -p "Do you want to insall Lisk? <y/N> " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then
+	lisk
+fi
 
 echo "All done, have fun :)"
